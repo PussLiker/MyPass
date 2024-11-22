6955547fc2464e86df93dce46c950a3dd501cf59
Вадим Клевцов <vadimklvcv1@gmail.com>
2024-11-22 23:42:33 +0300

    Добавлена модель шифрования и дешифровки

---
+++ mypass/Model/EncryptionModel.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.Text;
+using System.IO;
+
+namespace mypass.Model
+{
+    internal class EncryptionModel
+    {
+        //Создание массива байтов для ключа шифрования
+        private static byte[] GenerateKey(string password)
+        {
+
+            using (SHA512 sha512 = SHA512.Create())
+            {
+                byte[] fullKey = sha512.ComputeHash(Encoding.UTF8.GetBytes(password));
+                byte[] shortKey = new byte[32];
+                Array.Copy(fullKey, shortKey, 32);
+                return shortKey;
+            }
+            
+        }
+        //Генерация IV
+        private static byte[] GenerateIV()
+        {
+            using (var aes = Aes.Create())
+            {
+                aes.GenerateIV();
+                return aes.IV;
+            }
+        }
+        //Шифрование пароля
+        public static string Enscrypt(string password, string master_password)
+        {
+            byte[] key = GenerateKey(master_password);
+            byte[] iv = GenerateIV();
+
+            using (var aes = Aes.Create())
+            {
+                aes.Key = key;
+                aes.IV = iv;
+
+                using (var enscryptor = aes.CreateEncryptor(aes.Key, aes.IV))
+                using (var memoryStream = new MemoryStream())
+                {
+                    memoryStream.Write(iv, 0, iv.Length);
+
+                    using (var cryptoStream = new CryptoStream(memoryStream, enscryptor, CryptoStreamMode.Write))
+                    using (var writer = new StreamWriter(cryptoStream))
+                    {
+                        writer.Write(password);
+                    }
+                    return Convert.ToBase64String(memoryStream.ToArray());
+                }
+            }
+        }
+        //Дешифровка пароля
+        public static string Decrypt(string password, string master_password)
+        {
+            byte[] key = GenerateKey(master_password);
+            byte[] encryptPassword = Convert.FromBase64String(password);
+
+            using (var memoryStream = new MemoryStream(encryptPassword))
+            {
+                byte[] iv = new byte[16];
+                memoryStream.Read(iv, 0, iv.Length);
+
+                using (Aes aes = Aes.Create())
+                {
+                    aes.Key = key;
+                    aes.IV = iv;
+
+                    using (var decryptor = aes.CreateDecryptor(aes.Key, aes.IV))
+                    using (var cryptoStream = new CryptoStream(memoryStream, decryptor, CryptoStreamMode.Read))
+                    using (var reader = new StreamReader(cryptoStream))
+                    {
+                        return reader.ReadToEnd();
+                    }
+                }
+            }
+        }
+    }
+}
+
