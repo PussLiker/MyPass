8d3cfa475e0b9dc66abdebb9f822cf77246d95b4
Вадим Клевцов <vadimklvcv1@gmail.com>
2024-11-23 00:18:45 +0300

    Добавлена модель генерации паролей

---
+++ mypass/Model/PasswordGeneration.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ExceptionServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mypass.Model
+{
+    internal class PasswordGeneration
+    {
+        //Проверка доступности генерации
+        public static bool CheckExecute(bool isNeedUpcase, bool isNeedLowercase,
+    bool isNeedNumber, bool isNeedSpecialSymbol, string specialSymbols = null)
+        {
+            if (!isNeedUpcase && !isNeedLowercase && !isNeedNumber && !isNeedSpecialSymbol) return false;
+            if (isNeedSpecialSymbol && !isNeedUpcase && !isNeedLowercase && !isNeedNumber && specialSymbols.Length < 2) return false;
+            return true;
+        }
+        //Генерация пароля
+        public static string PasswordGenerate(int size, bool isNeedUpcase, bool isNeedLowercase,
+    bool isNeedNumber, bool isNeedSpecialSymbol, string specialSymbols = null)
+        {
+
+            const string upCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            const string lowerCase = "abcdefghijklmnopqrstuvwxyz";
+            const string numbers = "1234567890";
+
+            string masterString = "";
+            if (isNeedUpcase) masterString += upCase;
+            if (isNeedLowercase) masterString += lowerCase;
+            if (isNeedNumber) masterString += numbers;
+            if (isNeedSpecialSymbol) masterString += specialSymbols;
+
+            string password = "";
+
+            Random rand = new Random();
+
+            if (isNeedUpcase) password += upCase[rand.Next(upCase.Length)];
+            if (isNeedLowercase) password += lowerCase[rand.Next(lowerCase.Length)];
+            if (isNeedNumber) password += numbers[rand.Next(numbers.Length)];
+            if (isNeedSpecialSymbol) password += specialSymbols[rand.Next(specialSymbols.Length)];
+
+            while (password.Length < size)
+            {
+                password += masterString[rand.Next(masterString.Length)];
+            }
+
+            return Shuffle(password);
+        }
+        //Перемешка пароля
+        private static string Shuffle(string password)
+        {
+            Random rand = new Random();
+            char[] chars = password.ToCharArray();
+            for (int i = 0; i < chars.Length; i++)
+            {
+                int j = rand.Next(chars.Length);
+                (chars[i], chars[j]) = (chars[j], chars[i]);
+            }
+            return new string(chars);
+        }
+    }
+}
