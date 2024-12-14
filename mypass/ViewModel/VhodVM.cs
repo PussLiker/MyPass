4b94e492deb722c47fbd2c257c4e7584bd76d634
GoodVoid <98227501+GoodVoid@users.noreply.github.com>
2024-12-14 22:27:02 +0300

    Начало конца...

---
+++ mypass/ViewModel/VhodVM.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mypass.ViewModel
+{
+    internal class VhodVM
+    {
+        private object _vhodvm;
+
+        public object VhodimVM { get { return _vhodvm; } set { _vhodvm = value; } }
+
+
+    }
+}
