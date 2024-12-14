288c08a491f65f18f256fb4194f2ab20965d823a
Badim <vadimklvcv1@gmail.com>
2024-12-14 21:57:43 +0300

    Revert "Merge branch 'main' of https://github.com/yadernijhuesos/MyPass"
    This reverts commit 7056d10c857992f208e9a4671ffd1b891f01e8f0, reversing
    changes made to 917e13c18872f902a404d0263250a47d40a4105f.

---
+++ mypass/Model/Program (2).cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data.SQLite;
+using System.Globalization;
+
+namespace mypass.Model
+{
+    class Program
+    {
+        static void Programer(string[] args)
+        {
+            // Создать базу данных для клиента
+            string clientName = "Dima";
+            string password = "securepassword123";
+
+            DataBaseCreate.CreateEncryptedDatabase(clientName, password);
+        }
+    }
+}
