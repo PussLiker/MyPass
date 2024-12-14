917e13c18872f902a404d0263250a47d40a4105f
Badim <vadimklvcv1@gmail.com>
2024-12-14 21:56:10 +0300

    ХУЙ

---
+++ mypass/ViewModel/PassCheckVM.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using mypass.Model;
+
+namespace mypass.ViewModel
+{
+    internal class PassCheckVM : Utilities.ViewModelBase
+    {
+        private readonly PageModel _pageModel;
+
+        public PassCheckVM()
+        {
+            _pageModel = new PageModel();
+        }
+    }
+}
---
+++ mypass/ViewModel/PassGenVM.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using mypass.Model;
+using mypass.Utilities;
+using mypass.View;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows;
+using System.Threading;
+using System.Windows.Media;
+
+namespace mypass.ViewModel
+{
+    internal class PassGenVM : Utilities.ViewModelBase
+    {
+        private readonly PageModel _pageModel;
+        private CancellationTokenSource _cancellationTokenSource;
+        public ICommand CopyTextCommand { get; }
+        public ICommand PasswordGenerateCommand { get; }
+
+        private string _text;
+        public string Text
+        {
+            get => _text;
+            set
+            {
+                if (_text != value)
+                {
+                    _text = value;
+                    OnPropertyChanged(nameof(Text)); // Уведомляем View об изменении
+                }
+            }
+        }
+
+
+        private Brush _textColor = Brushes.White;
+        public Brush TextColor
+        {
+            get => _textColor;
+            set
+            {
+                if (_textColor != value)
+                {
+                    _textColor = value;
+                    OnPropertyChanged(nameof(TextColor));
+                }
+            }
+        }
+        
+        public ICommand IncreasePasswordLengthCommand { get; }
+        public ICommand DecreasePasswordLengthCommand { get; }
+
+        private int _passwordLength = 14;
+        public int PasswordLength
+        {
+            get => _passwordLength;
+            set
+            {
+                if (_passwordLength != value && value > 3 && value <= 99) // Ограничение 4-99
+                {
+                    _passwordLength = value;
+                    OnPropertyChanged(nameof(PasswordLength));
+                }
+            }
+        }
+        private bool _includeLowercase = true;
+        public bool IncludeLowercase
+        {
+            get => _includeLowercase;
+            set
+            {
+                if (_includeLowercase != value)
+                {
+                    _includeLowercase = value;
+                    OnPropertyChanged(nameof(IncludeLowercase));
+                }
+            }
+        }
+
+        private bool _includeUppercase = true;
+        public bool IncludeUppercase
+        {
+            get => _includeUppercase;
+            set
+            {
+                if (_includeUppercase != value)
+                {
+                    _includeUppercase = value;
+                    OnPropertyChanged(nameof(IncludeUppercase));
+                }
+            }
+        }
+
+        private bool _includeDigits = true;
+        public bool IncludeDigits
+        {
+            get => _includeDigits;
+            set
+            {
+                if (_includeDigits != value)
+                {
+                    _includeDigits = value;
+                    OnPropertyChanged(nameof(IncludeDigits));
+                }
+            }
+        }
+
+        private bool _includeSpecialCharacters = true;
+        public bool IncludeSpecialCharacters
+        {
+            get => _includeSpecialCharacters;
+            set
+            {
+                if (_includeSpecialCharacters != value)
+                {
+                    _includeSpecialCharacters = value;
+                    OnPropertyChanged(nameof(IncludeSpecialCharacters));
+                }
+            }
+        }
+
+        private string _specialCharacters = "!@#$%^&*()";
+        public string SpecialCharacters
+        {
+            get => _specialCharacters;
+            set
+            {
+                if (_specialCharacters != value)
+                {
+                    _specialCharacters = value;
+                    OnPropertyChanged(nameof(SpecialCharacters));
+                }
+            }
+        }
+
+
+
+        public PassGenVM()
+        {
+            // Создаем команду
+            CopyTextCommand = new RelayCommand(CopyTextToClipboard);
+            PasswordGenerateCommand = new RelayCommand(SetPass);
+            _pageModel = new PageModel();
+
+            // Пример инициализации текста
+            PasswordGenerateCommand = new RelayCommand(ExecutePasswordAnimation);
+            ExecutePasswordAnimation(new object());
+
+            IncreasePasswordLengthCommand = new RelayCommand(_ => PasswordLength++, _ => PasswordLength < 99);
+            DecreasePasswordLengthCommand = new RelayCommand(_ => PasswordLength--, _ => PasswordLength > 4);
+
+
+        }
+        private void SetPass(object sender)
+        {
+            // Пример инициализации текста
+            Text = PasswordGeneration.PasswordGenerate(10, true, false, true, false, "!");
+
+        }
+
+        private async void ExecutePasswordAnimation(object obj)
+        {
+            _cancellationTokenSource?.Cancel();
+            _cancellationTokenSource = new CancellationTokenSource();
+
+            try
+            {
+                await AnimatePasswordGeneration(_cancellationTokenSource.Token);
+            }
+            catch (TaskCanceledException)
+            {
+
+            }
+        }
+
+        private async Task AnimatePasswordGeneration(CancellationToken cancellationToken)
+        {
+
+
+            for (int i = 0; i < 6; i++)
+            {
+                cancellationToken.ThrowIfCancellationRequested();
+
+                // Генерация случайного промежуточного пароля
+                Text = PasswordGeneration.PasswordGenerate(PasswordLength, IncludeUppercase, IncludeLowercase, IncludeDigits, IncludeSpecialCharacters, SpecialCharacters);
+                TextColor = Brushes.DarkGray;
+
+                // Имитируем задержки между сменой паролей
+                await Task.Delay(50, cancellationToken);
+            }
+
+            // Финальный белый пароль
+            Text = PasswordGeneration.PasswordGenerate(PasswordLength, IncludeUppercase, IncludeLowercase, IncludeDigits, IncludeSpecialCharacters, SpecialCharacters);
+            TextColor = Brushes.White;
+        }
+
+
+        private void CopyTextToClipboard(object text)
+        {
+            if (!string.IsNullOrWhiteSpace(text?.ToString()))
+            {
+                Clipboard.SetText(text.ToString());
+            }
+        }
+
+    }
+
+
+}
+        
+
