303d300a42a687834ae6eae71061a71d9b9f0dbd
GoodVoid <98227501+GoodVoid@users.noreply.github.com>
2024-12-18 13:53:01 +0300

    Pop Up

--- mypass/ViewModel/VhodVM.cs
+++ mypass/ViewModel/VhodVM.cs
@@ -3,15 +3,45 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using mypass.View;
+using mypass.Utilities;
 
 namespace mypass.ViewModel
 {
-    internal class VhodVM
+    internal class VhodVM : ViewModelBase
     {
         private object _vhodvm;
 
-        public object VhodimVM { get { return _vhodvm; } set { _vhodvm = value; } }
+        public object VhodimVM
+        {
+            get { return _vhodvm; }
+            set { _vhodvm = value; OnPropertyChanged(); }
 
+        }
+        private string _pass;
+        private string _log;
+
+        public string Log
+        {
+            get => _log;
+            set 
+            {
+                if (_pass != value)
+                { _log = value; OnPropertyChanged(); }
+            }
+        }
+        public string Pass
+        {
+            get => _pass;
+            set
+            {
+                if (_pass != value)
+                {
+                    _pass = value;
+                    OnPropertyChanged(nameof(Pass)); // Уведомляем View об изменении
+                }
+            }
+        }
 
     }
 }
