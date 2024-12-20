6a607bae8f00d41ff0d2cba50c718235aede8f69
JastLine <159622807+Jastline@users.noreply.github.com>
2024-12-21 01:12:32 +0300

    Логирование чудь-чудь

--- mypass/Model/UserDB.cs
+++ mypass/Model/UserDB.cs
@@ -50,20 +50,24 @@ namespace mypass.Model
         // Метод для добавления нового пользователя
         public bool AddUser(int idUser, string name, string masterPasswordHash, string salt)
         {
+            InitTransaction("Вызов метода 'AddUser'");
             OpenConnection();
 
             string query = "INSERT INTO Users (IdUser, Username, MasterPasswordHash, Salt) VALUES (@IdUsers, @Username, @MasterPasswordHash, @Salt);";
+            MessageError($"Создание запроса: {query}");
             int affectedRows;
 
             using (var command = _connection.CreateCommand())
             {
                 command.CommandText = query;
+                MessageError($"Вызов команд для добавления");
                 command.Parameters.AddWithValue("@IdUser", idUser);
                 command.Parameters.AddWithValue("@Username", name);
                 command.Parameters.AddWithValue("@MasterPasswordHash", masterPasswordHash);
                 command.Parameters.AddWithValue("@Salt", salt);
 
                 affectedRows = command.ExecuteNonQuery();
+                MessageError($"Значение переменной после завершения команд: {affectedRows}");
             }
 
             CloseConnection();
@@ -71,32 +75,38 @@ namespace mypass.Model
             // Обновляем поля класса, если обновление прошло успешно
             if (affectedRows > 0)
             {
+                MessageError($"Применение полученных значений к полям класса");
                 _id = idUser;
                 _username = name;
                 _masterPasswordHash = masterPasswordHash;
                 _salt = salt;
             }
 
+            CloseTransaction();
             return affectedRows > 0;
         }
 
         // Метод для обновления данных пользователя
         public bool UpdateUser(int idUser, string newName, string newPasswordHash, string newSalt)
         {
+            InitTransaction("Вызов метода 'UpdateUser'");
             OpenConnection();
 
             string query = "UPDATE Users SET Username = @Username, MasterPasswordHash = @MasterPasswordHash, Salt = @Salt WHERE IdUser = @IdUser;";
+            MessageError($"Создание запроса: {query}");
             int affectedRows;
 
             using (var command = _connection.CreateCommand())
             {
                 command.CommandText = query;
+                MessageError($"Вызов команд для обновления");
                 command.Parameters.AddWithValue("@IdUser", idUser);
                 command.Parameters.AddWithValue("@Username", newName);
                 command.Parameters.AddWithValue("@MasterPasswordHash", newPasswordHash);
                 command.Parameters.AddWithValue("@Salt", newSalt);
 
                 affectedRows = command.ExecuteNonQuery();
+                MessageError($"Значение переменной после завершения команд: {affectedRows}");
             }
 
             CloseConnection();
@@ -110,53 +120,41 @@ namespace mypass.Model
                 _salt = newSalt;
             }
 
+            CloseTransaction();
             return affectedRows > 0;
         }
 
-        // Метод удаления пользователя и базы данных, если он последний
-        public bool RemoveUser(string name)
+        // Метод удаления ,базы данных (тк одна бд для одного пользователя)
+        public bool RemoveUser()
         {
-            OpenConnection();
+            InitTransaction("Вызов метода 'RemoveUser'");
+            bool result = false;
 
-            string checkQuery = "SELECT COUNT(*) FROM Users;";
-            int userCount;
-
-            using (var command = _connection.CreateCommand())
+            try
             {
-                command.CommandText = checkQuery;
-                userCount = Convert.ToInt32(command.ExecuteScalar());
+                File.Exists(_databasePath);
+                result = true;
+            }
+            catch (Exception)
+            {
+                result = false;
             }
 
-            string deleteQuery = "DELETE FROM Users WHERE Username = @Username;";
-            int affectedRows = 0;
-
-            using (var command = _connection.CreateCommand())
-            {
-                command.CommandText = deleteQuery;
-                command.Parameters.AddWithValue("@Username", name);
-                affectedRows = command.ExecuteNonQuery();
-            }
-
-            CloseConnection();
-
-            // Если это был последний пользователь, удаляем всю базу данных
-            if (userCount == 1 && affectedRows > 0)
-            {
-                File.Delete(_databasePath);
-                Console.WriteLine("Последний пользователь удалён. База данных также удалена.");
-            }
-
-            return affectedRows > 0;
+            MessageError($"Резульат попытки удаления БД: {result}");
+            CloseTransaction();
+            return result;
         }
 
         // Метод для получения полной информации о пользователе
         public Dictionary<string, string> GetUserData(int idUser)
         {
+            InitTransaction("Вызов метода 'GetUserData'");
             Dictionary<string, string> userDataDictionary = new Dictionary<string, string>();
 
             OpenConnection();
 
             string query = "SELECT IdUser, Username, MasterPasswordHash, Salt FROM Users WHERE IdUser = @IdUser;";
+            MessageError($"Создание запроса: {query}");
 
             using (var command = _connection.CreateCommand())
             {
@@ -167,11 +165,13 @@ namespace mypass.Model
                 {
                     if (reader.Read())
                     {
+                        MessageError($"Заполнение полей полученными данными");
                         _id = Convert.ToInt32(reader["IdUser"]);
                         _username = reader["Username"].ToString();
                         _masterPasswordHash = reader["MasterPasswordHash"].ToString();
                         _salt = reader["Salt"].ToString();
 
+                        MessageError($"Добавление в словарь полученных данных");
                         userDataDictionary.Add("IdUser", _id.ToString());
                         userDataDictionary.Add("Username", _username);
                         userDataDictionary.Add("MasterPasswordHash", _masterPasswordHash);
@@ -179,11 +179,12 @@ namespace mypass.Model
                     }
                     else
                     {
-                        Console.WriteLine("Пользователь не найден.");
+                        MessageError($"Пользователь не найден");
                     }
                 }
             }
 
+            CloseTransaction();
             CloseConnection();
             return userDataDictionary;
         }
