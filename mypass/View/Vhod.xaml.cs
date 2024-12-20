0c7d9fb0d92c080ecfb6471befa254a2256be2e1
JastLine <159622807+Jastline@users.noreply.github.com>
2024-12-21 01:21:50 +0300

    починка слияния
    123

--- mypass/View/Vhod.xaml.cs
+++ mypass/View/Vhod.xaml.cs
@@ -13,34 +13,14 @@ using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using mypass.ViewModel;
 
-<<<<<<<< HEAD:mypass/View/Vhod.xaml.cs
 namespace mypass.View
 {
-    /// <summary>
     /// Логика взаимодействия для Vhod.xaml
-    /// </summary>
     public partial class Vhod : UserControl
     {
         public Vhod()
         {
             InitializeComponent();
         }
-========
-
-
-namespace mypass.View
-{
-    /// <summary>
-    /// Логика взаимодействия для MainAuthWindow.xaml
-    /// </summary>
-    public partial class MainAuthWindow : Window { 
-
-        public MainAuthWindow()
-            {
-                InitializeComponent();
-            }
-        
-    
->>>>>>>> DbTest:mypass/View/MainAuthWindow.xaml.cs
     }
 }
