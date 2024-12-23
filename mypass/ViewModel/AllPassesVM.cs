135b626088738af7f14a02cdf3ccdaed660b0bb7
Badim <vadimklvcv1@gmail.com>
2024-12-23 03:45:37 +0300

    Squashed commit of the following:
    commit 35becc4e67f9594c0196ef2fff3f26c1ee91f0af
    Author: Badim <vadimklvcv1@gmail.com>
    Date:   Mon Dec 23 03:42:50 2024 +0300
    
        ЭЭЭ сделал кнопки
    
        Добавил кнопку показа, скрытия пароля, оно почти работает
    
    commit bb76de123da2d5605d4b762a43a8c6ed5309a19f
    Author: Badim <vadimklvcv1@gmail.com>
    Date:   Mon Dec 23 00:54:03 2024 +0300
    
        Сделал кнопки
    
        Сделал кнопки копирования и перехода на сайт

--- mypass/ViewModel/AllPassesVM.cs
+++ mypass/ViewModel/AllPassesVM.cs
@@ -1,36 +1,67 @@
-﻿using mypass.Model;
-﻿using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Input;
+using mypass.Model;
+using mypass.Utilities;
 
 namespace mypass.ViewModel
 {
     internal class AllPassesVM : Utilities.ViewModelBase
     {
-        private readonly PageModel _pageModel;
         public ObservableCollection<Account> Accounts { get; set; }
+        public ICommand OpenLinkCommand { get; }
+        public ICommand CopyEmailCommand { get; }
+        public ICommand CopyPasswordCommand { get; }
+        public ICommand TogglePasswordVisibilityCommand { get; }
+
         public AllPassesVM()
         {
-            _pageModel = new PageModel();
+            // Привязываем команды к методам с учетом параметров
+            CopyEmailCommand = new RelayCommand(CopyEmail);
+            CopyPasswordCommand = new RelayCommand(CopyPassword);
+            TogglePasswordVisibilityCommand = new RelayCommand(TogglePasswordVisibility); // Используем RelayCommand без параметра типа
+
+            OpenLinkCommand = new RelayCommand(parameter =>
+            {
+                if (parameter is Account account)
+                {
+                    account.OpenLink();
+                }
+            });
 
             Accounts = new ObservableCollection<Account>
             {
-            new Account { Username = "User1", Email = "user1@example.com", Password ="afskjhb"},
-            new Account { Username = "User2", Email = "user2@example.com", Password ="afskjhb"},
-            new Account { Username = "User1", Email = "user1@example.com", Password ="afskjhb"},
-            new Account { Username = "User2", Email = "user2@example.com", Password ="afskjhb"},
-            new Account { Username = "User1", Email = "user1@example.com", Password ="afskjhb"},
-            new Account { Username = "User2", Email = "user2@example.com", Password ="afskjhb"},
-            new Account { Username = "User1", Email = "user1@example.com", Password ="afskjhb"},
-            new Account { Username = "User2", Email = "user2@example.com", Password ="afskjhb"},
-            new Account { Username = "User1", Email = "user1@example.com", Password ="afskjhb"},
-            new Account { Username = "User2", Email = "user2@example.com", Password ="afskjhb"},
-            // ...
+                new Account { Username = "vk.com", Email = "user1@example.com", Password = "password" },
+                new Account { Username = "not-a-url.ru", Email = "user2@example.com", Password = "password" },
+                new Account { Username = "", Email = "user1@example.com", Password = "password" },
+                new Account { Username = "https://not-a-url", Email = "user2@example.com", Password = "password" },
+                new Account { Username = "http://pornhub.com", Email = "user1@example.com", Password = "password" },
+                new Account { Username = "ghsd;l'kjsdfghladfgb;ljh", Email = "user2@example.com", Password = "password" }
             };
+        }
+
+        private void CopyEmail(object parameter)
+        {
+            if (parameter is Account account)
+            {
+                Clipboard.SetText(account.Email); // Копирует Email в буфер обмена
+            }
+        }
+
+        private void CopyPassword(object parameter)
+        {
+            if (parameter is Account account)
+            {
+                Clipboard.SetText(account.Password); // Копирует Password в буфер обмена
+            }
+        }
+
+        private void TogglePasswordVisibility(object parameter)
+        {
+            if (parameter is Account account)
+            {
+                account.IsPasswordVisible = !account.IsPasswordVisible; // Переключаем видимость пароля для конкретной учетной записи
+            }
         }
     }
 }
